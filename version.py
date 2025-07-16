5aa129ca24de957e1513051b914be743160d1df4
Paolo D'Elia <paolo.delia99@gmail.com>
2025-07-16 14:49:02 +0200

    Update version.py
    printing for debugging purposes

--- version.py
+++ version.py
@@ -6,8 +6,10 @@ def get_latest_tag() -> str:
             ["git", "describe", "--tags", "--abbrev=0"],
             stderr=subprocess.STDOUT
         ).strip().decode()
+        print(f"Latest tag: {tag}")
         return tag
     except subprocess.CalledProcessError:
+        print("Tag not found falling back to v0.0.0")
         return "v0.0.0"  # Default if no tag is found or git fails
 
 __version__ = get_latest_tag()
